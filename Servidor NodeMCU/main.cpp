566f93eaff4240a72b4663628e0ad23b3055aa00
João Ricardo Criminácio <joaoricardocriminacio@gmail.com>
2024-11-05 20:45:09 -0300

    Servidor NodeMCU2

--- Servidor NodeMCU/main.cpp
+++ Servidor NodeMCU/main.cpp
@@ -1,38 +1,54 @@
+#include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid     = "Galaxy M515ABA";
-const char* password = "";
+const char* ssid = "Galaxy M515ABA";
+const char* password = "pediuasenha=mamou";
 
 ESP8266WebServer server(80);
 
-const int output5 = 5;//D1
+const int led = LED_BUILTIN;
 
-//Você precisa colocar o ip da sua faixa
-IPAddress local_IP(192,168,227,241); //Troque o IP
+IPAddress local_IP(192,168,227,241);
 IPAddress gateway(192,168,227,217);
 IPAddress subnet(255,255,255,0);
 
+void configureWiFi();
 void handleRoot();
-void handleOn();
-void handleOff();
+void turnOnLED();
+void turnOffLED();
+
+void configureWiFi() {
+  WiFi.mode(WIFI_STA);
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("Falha ao configurar IP.");
+  }
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWi-Fi conectado!");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void handleRoot() {
   String html = "<!DOCTYPE html><html>";
   html += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
   html += "<link rel=\"icon\" href=\"data:,\">";
-  html += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}";
-  html += ".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;";
+  html += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center; background-color: #4b5563; color: #f8fafc;}";
+  html += ".button { background-color: #22c55e; border: none; color: white; padding: 16px 40px;";
   html += "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}";
-  html += ".button2 {background-color: #77878A;}</style></head>";
-  html += "<body><h1>ESP8266 Web Server</h1>";
-  html += "<h2>Interruptor</h2>";
-  html += "<button onclick=\"toggleLED('off')\" class=\"button\">ON</button>";
-  html += "<button onclick=\"toggleLED('on')\" class=\"button button2\">OFF</button>";
+  html += ".button2 {background-color: #ef4444;}</style></head>";
+  html += "<body><h1>Turning on LED of NodeMCU</h1>";
+  html += "<h2>Buttons</h2>";
+  html += "<button onclick=\"toggleLED('on')\" class=\"button\">ON</button>";
+  html += "<button onclick=\"toggleLED('off')\" class=\"button button2\">OFF</button>";
   html += "<script>";
   html += "function toggleLED(state) {";
   html += "  var xhr = new XMLHttpRequest();";
-  html += "  xhr.open('GET', '/5/' + state, true);";
+  html += "  xhr.open('GET', '/' + state, true);";
   html += "  xhr.send();";
   html += "}";
   html += "</script>";
@@ -40,40 +56,56 @@ void handleRoot() {
   server.send(200, "text/html", html);
 }
 
-void handleOn() {
-  digitalWrite(output5, HIGH);
+void turnOnLED() {
+  digitalWrite(led, LOW);
   server.send(200, "text/plain", "LED Ligado!");
+  Serial.println("LED foi ligado.");
 }
 
-void handleOff() {
-  digitalWrite(output5, LOW);
+void turnOffLED() {
+  digitalWrite(led, HIGH);
   server.send(200, "text/plain", "LED Desligado!");
+  Serial.println("LED foi desligado.");
 }
 
-void setup() {
+void setup(){
   Serial.begin(9600);
-  pinMode(output5, OUTPUT);
-  digitalWrite(output5, LOW);
- 
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("STA Failed to configure");
-  }
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
- 
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+
+  ArduinoOTA.setPassword("vaiCurintia");
+
+  configureWiFi();
+
+  ArduinoOTA.onStart([](){
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([](){
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total){
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  /* Função assíncrona que lida com os erros */
+  ArduinoOTA.onError([](ota_error_t error){
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+  });
+
   server.on("/", HTTP_GET, handleRoot);
-  server.on("/5/on", HTTP_GET, handleOn);
-  server.on("/5/off", HTTP_GET, handleOff);
+  server.on("/on", HTTP_GET, turnOnLED);
+  server.on("/off", HTTP_GET, turnOffLED);
   server.begin();
+
+  ArduinoOTA.begin();
+  Serial.println("Pronto para uso.");
 }
 
 void loop(){
+  ArduinoOTA.handle();
   server.handleClient();
 }
